a4dbc3b3b025731e21840ea5a2d486a76088676b
sakulstra <lukasstrassel@googlemail.com>
2021-06-19 21:55:56 +0200

    fix: update deps

--- app/user-reserves/queries/getStats.ts
+++ app/user-reserves/queries/getStats.ts
@@ -2,19 +2,18 @@ import { resolver } from "blitz"
 import dayjs from "dayjs"
 import db, { Prisma } from "db"
 
-interface GetFormattedUserReservesInput
-  extends Pick<Prisma.FormattedUserReserveFindManyArgs, "where"> {}
+interface GetUserReservesInput extends Pick<Prisma.UserReserveFindManyArgs, "where"> {}
 
-export default resolver.pipe(async ({ where }: GetFormattedUserReservesInput) => {
+export default resolver.pipe(async ({ where }: GetUserReservesInput) => {
   const [reserves, emptyReserves, activeReserves24h, activeReserves30d] = await Promise.all([
-    db.formattedUserReserve.count({ where }),
-    db.formattedUserReserve.count({
-      where: { scaledATokenBalance: 0, currentTotalDebt: 0, ...where },
+    db.userReserve.count({ where }),
+    db.userReserve.count({
+      where: { scaledATokenBalance: "0", currentTotalDebt: "0", ...where },
     }),
-    db.formattedUserReserve.count({
+    db.userReserve.count({
       where: { lastUpdateTimestamp: { gte: dayjs().unix() - 60 * 60 * 24 }, ...where },
     }),
-    db.formattedUserReserve.count({
+    db.userReserve.count({
       where: { lastUpdateTimestamp: { gte: dayjs().unix() - 60 * 60 * 24 * 30 }, ...where },
     }),
   ])
--- app/user-reserves/queries/getUserReserves.ts
+++ app/user-reserves/queries/getUserReserves.ts
@@ -1,18 +1,18 @@
 import { paginate, resolver } from "blitz"
 import db, { Prisma } from "db"
 
-interface GetFormattedUserReservesInput
-  extends Pick<Prisma.FormattedUserReserveFindManyArgs, "where" | "orderBy" | "skip" | "take"> {}
+interface GetUserReservesInput
+  extends Pick<Prisma.UserReserveFindManyArgs, "where" | "orderBy" | "skip" | "take"> {}
 
 export default resolver.pipe(
-  async ({ where, orderBy, skip = 0, take = 100 }: GetFormattedUserReservesInput) => {
+  async ({ where, orderBy, skip = 0, take = 100 }: GetUserReservesInput) => {
     // TODO: in multi-tenant app, you must add validation to ensure correct tenant
     const { items, hasMore, nextPage, count } = await paginate({
       skip,
       take,
-      count: () => db.formattedUserReserve.count({ where }),
+      count: () => db.userReserve.count({ where }),
       query: (paginateArgs) =>
-        db.formattedUserReserve.findMany({
+        db.userReserve.findMany({
           ...paginateArgs,
           where,
           orderBy,
